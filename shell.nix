adab52d1d49dc630494192c8653262cfcb94d56b
acidicneko <ayush7788899@gmail.com>
2025-12-13 09:54:42 +0530

    fix: nix version

--- shell.nix
+++ shell.nix
@@ -1,7 +1,4 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
