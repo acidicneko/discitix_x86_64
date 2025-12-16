b5ead9d26ec79bad8e33b4b5bdc3db975b0a3c7c
acidicneko <ayush7788899@gmail.com>
2025-12-16 19:49:30 +0530

    add: exit command

--- src/libk/shell.c
+++ src/libk/shell.c
@@ -7,7 +7,7 @@
 #include <libk/utils.h>
 #include <mm/pmm.h>
 
-void execute(char **argv, int argc) {
+int execute(char **argv, int argc) {
   if (!strcmp(argv[0], "uname")) {
     printf("Discitix Kernel x86_64; Build: %s\n\n", __DATE__);
   } else if (!strcmp(argv[0], "sysfetch")) {
@@ -37,7 +37,7 @@ void execute(char **argv, int argc) {
     superblock_t *sb = vfs_get_root_superblock();
     if (!sb || !sb->root || !sb->root->inode || !sb->root->inode->private) {
       printf("No filesystem mounted.\n\n");
-      return;
+      return -1;
     }
     dentry_t *d = sb->root;
     d = d->next; // skip root itself
@@ -49,33 +49,35 @@ void execute(char **argv, int argc) {
   } else if(!strcmp(argv[0], "cat")) {
     if (argc < 2) {
       printf("Usage: cat <filename>\n\n");
-      return;
+      return -1;
     }
     superblock_t *sb = vfs_get_root_superblock();
     if (!sb || !sb->root || !sb->root->inode) {
       printf("No filesystem mounted.\n\n");
-      return;
+      return -1;
     }
     sb->root->inode->is_directory = 1; // ensure root is marked as directory
     inode_t *file_inode = NULL;
     if (vfs_lookup(sb->root->inode, argv[1], &file_inode) != 0 || !file_inode) {
       printf("ayu.sh: cat: %s: No such file\n\n", argv[1]);
-      return;
+      return -1;
     }
     file_t *file = NULL;
     if (vfs_open(&file, file_inode, 0) != 0 || !file) {
       printf("ayu.sh: cat: %s: Failed to open file\n\n", argv[1]);
-      return;
+      return -1;
     }
     char buffer[1024];
     long bytes_read = vfs_read(file, buffer, sizeof(buffer) - 1);
     if (bytes_read < 0) {
       printf("ayu.sh: cat: %s: Failed to read file\n\n", argv[1]);
-      return;
+      return -1;
     }
     buffer[bytes_read] = '\0'; // null-terminate the buffer
     printf("%s\n", buffer);
     vfs_close(file);
+  } else if(!strcmp(argv[0], "exit")) {
+    return 1;
   } else {
     printf("ayu.sh: %s: unknown command\n\n", argv[0]);
   }
@@ -93,7 +95,9 @@ void init_shell() {
       argc++;
       argv[argc] = strtok(0, " \t");
     }
-    execute(argv, argc);
+    if (execute(argv, argc) == 1) {
+      break;
+    }
     argc = 0;
   }
 }
