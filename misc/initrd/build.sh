970ac443e7080175b90203cd39a6fca0c59b7c43
acidicneko <ayush7788899@gmail.com>
2026-02-07 10:58:49 +0530

    add: build script for initrd

---
+++ misc/initrd/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE_PATH="misc/initrd/initrd.img"
+
+FILES=(
+    # (source file) (path in initrd)
+    shell.nix shell.nix 
+    .gitmodules .gitmodules 
+    compile_flags.txt compile_flags.txt 
+    misc/default.psf font.psf 
+    userland/build/hello hello 
+    userland/build/cat cat 
+    userland/build/sh sh 
+    userland/build/ls ls 
+    userland/build/dbgln dbgln
+    userland/build/echo echo
+)
+
+stripFS/build/stripctl $IMAGE_PATH "${FILES[@]}"
