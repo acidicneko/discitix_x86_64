bb1893e8f431fe75e2b0c039f5d6231b42eb4c3e
acidicneko <ayush7788899@gmail.com>
2026-02-07 10:58:04 +0530

    add: serial as sr0 virtual device

--- src/drivers/serial.c
+++ src/drivers/serial.c
@@ -3,9 +3,19 @@
 #include <libk/stdio.h>
 #include <libk/utils.h>
 #include <stdbool.h>
+#include <kernel/vfs/vfs.h>
+#include <mm/liballoc.h>
+#include <libk/string.h>
 
 uint16_t def_port = 0;
 bool initialized = false;
+
+struct file_operations serial_file_ops = {
+    .open = NULL,
+    .close = NULL,
+    .read = NULL,
+    .write = serial_write
+};
 
 int serial_init(uint16_t port){
     outb(port + 1, 0x00);
@@ -49,4 +59,43 @@ void serial_printf(char* fmt, ...){
     va_start(args, fmt);
     __vsprintf__(fmt, args, serial_putchar, serial_puts);
     va_end(args);
+}
+
+
+long serial_write(file_t* file, const void* buf, size_t count, uint64_t off){
+    (void)file;
+    (void)off;
+    const char* cbuf = (const char*)buf;
+    for(size_t i = 0; i < count; i++){
+        serial_putchar(cbuf[i]);
+    }
+    return (long)count;
+}
+
+
+void init_serial_device(){
+    inode_t* serial_inode = NULL;
+    superblock_t* root_sb = vfs_get_root_superblock();
+    if(!root_sb){
+        dbgln("Serial: Failed to get root superblock!\n\r");
+        return;
+    }
+    serial_inode = (inode_t*)kmalloc(sizeof(inode_t));
+    memset(serial_inode, 0, sizeof(inode_t));
+    serial_inode->atime = 0;
+    serial_inode->ctime = 0;
+    serial_inode->is_directory = 0;
+    serial_inode->type = FT_CHR;
+    serial_inode->size = 0;
+    serial_inode->ino = 5000;
+    serial_inode->f_ops = &serial_file_ops;
+    serial_inode->mode = 2;
+    dentry_t *d = (dentry_t *)kmalloc(sizeof(dentry_t));
+    memset(d, 0, sizeof(dentry_t));
+    strncpy(d->name, "sr0", 4);
+    d->name[NAME_MAX - 1] = '\0';
+    d->inode = serial_inode;
+    d->parent = root_sb->root;
+    d->next = root_sb->root->next;
+    root_sb->root->next = d;
 }
--- src/include/drivers/serial.h
+++ src/include/drivers/serial.h
@@ -7,12 +7,15 @@
 #include <stdint.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <kernel/vfs/vfs.h>
 
 int serial_init(uint16_t port);
 bool is_serial_initialized();
-void serial_write(const char* data);
 void serial_putchar(char c);
 void serial_puts(const char* str);
 void serial_printf(char* fmt, ...);
+long serial_write(file_t* file, const void* buf, size_t count, uint64_t off);
+void init_serial_device();
 
 #endif
