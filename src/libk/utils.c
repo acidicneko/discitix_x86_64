6e42692f9d776f228b798899885eaf2be8113570
acidicneko <ayush7788899@gmail.com>
2026-02-07 11:21:53 +0530

    change: sysfetch ASCII art

--- src/libk/utils.c
+++ src/libk/utils.c
@@ -6,16 +6,17 @@
 #include <mm/pmm.h>
 
 void sysfetch() {
-  printf("\033[34m ____  \n");
-  printf("\033[34m|  _ \\\t\033[1;37mKernel: \033[0mDiscitix\n");
-  printf("\033[34m| | | |\t\033[1;37mBuild: \033[0m%s\n", __DATE__);
-  printf("\033[34m| |_| |\t\033[1;37mMemory: \033[0m%ul/%ul MB\n",
+  /* ASCII art + info */
+  printf("\033[34m '---' \t\033[1;37mKernel: \033[0mDiscitix\n");
+  printf("\033[34m (O,O) \t\033[1;37mBuild:  \033[0m%s\n", __DATE__);
+  printf("\033[34m /)_)\t\033[1;37mMemory: \033[0m%lu/%lu MB\n",
          get_free_physical_memory() / 1024 / 1024,
          get_total_physical_memory() / 1024 / 1024);
-  printf("\033[34m|____/\n\033[0m\n");
+  printf("\033[34m  \"\"  \n\033[0m\n");
 
+  /* Color palette */
   printf("\033[40m  \033[41m  \033[42m  \033[43m  \033[44m  \033[45m  \033[46m "
-         " \033[47m \n");
+         " \033[47m  \n");
   printf("\033[21;40m  \033[21;41m  \033[21;42m  \033[21;43m  \033[21;44m  "
          "\033[21;45m  \033[21;46m  \033[21;47m  \033[0m\n");
 }
